3e87c109537112026a8cb1aae4ca629b7a5d82a7
Michael Butler <mbutler@tumblr.com>
2022-03-12 18:53:46 -0500

    Add TimeUtil

---
+++ src/TimeUtil.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Butler\PhpHelpers;
+
+class TimeUtil
+{
+    public const SECONDS_PER_HOUR = 3600;
+
+    public const SECONDS_PER_DAY = 86400;
+
+    public const SECONDS_PER_WEEK = 604800;
+}
